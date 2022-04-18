c0c3c0ea46dbe5bcd4b82bb0610fd4710e952a84
choco_chanel <jh780465@naver.com>
2022-04-18 11:31:36 +0900

    javascript export and import

---
+++ javascript/0413/daynight.js
@@ -0,0 +1,42 @@
+function day() {
+    document.querySelector('body').style.backgroundColor = 'black';
+    // $('body').css('background-color', 'black');
+    document.querySelector('body').style.color = 'white';
+    // $('body').css('color', 'white');
+}
+
+function night() {
+    document.querySelector('body').style.backgroundColor = 'white';
+    // $('body').css('background-color', 'white');
+    document.querySelector('body').style.color = 'black';
+    // $('body').css('color', 'black');
+
+    // $('body').css('background-color', 'white').css('color', 'black');
+
+    // let a_tags = document.querySelectorAll('a');
+}
+
+export function daynight() {
+    let a_tags = document.querySelectorAll('a');
+                
+    if (this.value === 'night') {
+
+        night();
+        
+        for (let a of a_tags){
+            a.style.color = 'white';
+        }
+        this.value = 'day';
+    }
+
+    else if (this.value === 'day') {
+        
+        day();
+
+        for (let a of a_tags){
+            a.style.color = 'black';
+        }
+        // $('a').css('color', 'black');
+        this.value = 'night';
+    }
+}
