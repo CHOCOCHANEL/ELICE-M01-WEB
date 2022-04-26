fb7c8f6a487e3a853e25c11684130d51676d921d
choco_chanel <jh780465@naver.com>
2022-04-26 14:15:21 +0900

    user address form

--- 0426/async-search-user-address-form/App.js
+++ 0426/async-search-user-address-form/App.js
@@ -1,3 +1,4 @@
+import "./app.css";
 import UserInput from './UserInput.js';
 
 const App = () => {
@@ -6,6 +7,23 @@ const App = () => {
     const submitButton = document.getElementById("submit");
     const result = document.getElementById("result");
     const component = UserInput();
+
+    usernameForm.addEventListener('submit', e => {
+            e.preventDefault();
+
+            component.searchAddress().then((response) => {
+                if (component.getError()){
+                    result.innerHTML = component.getError();
+                    return;
+                }
+
+                result.innerHTML = response.address;
+            });
+    });
+
+    usernameInput.addEventListener('input', e => {
+        component.setValue(e.target.value);
+    });
 }
 
 export default App;
